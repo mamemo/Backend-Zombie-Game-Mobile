5e2c2b49bdee51d8567c4de14c4ffeef661383b2
Mauro <mauromm1996@gmail.com>
2018-05-24 18:15:49 -0600

    Adding get all, structure

---
+++ queries.js
@@ -0,0 +1,41 @@
+var promise = require('bluebird');
+
+var options = {
+  // Initialization Options
+  promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var connectionString = 'postgres://postgres:admin@localhost:5432/puppies';
+var db = pgp(connectionString);
+
+// add query functions
+
+module.exports = {
+  getAllPuppies: getAllPuppies,
+  getSinglePuppy: getSinglePuppy,
+  createPuppy: createPuppy,
+  updatePuppy: updatePuppy,
+  removePuppy: removePuppy
+};
+
+
+function getAllPuppies(req, res, next) {
+  db.any('select * from pups')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ALL puppies'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function getSinglePuppy(req, res, next) {};
+function createPuppy(req, res, next){};
+function updatePuppy(req, res, next) {};
+function removePuppy(req, res, next) {};
