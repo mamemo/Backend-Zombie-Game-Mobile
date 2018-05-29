3ff31e456db404f76fb0ad487555f1aa82d6e250
Mauro <mauromm1996@gmail.com>
2018-05-29 04:43:26 -0600

    Update db.js

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const options = {
 };
 
 const pgp = require('pg-promise')(options);
-const connectionString = 'postgres://postgres:admin@localhost:5432/zombies';
+const connectionString = 'postgres://postgres:admin123@zombies.crehatb3pukn.us-east-2.rds.amazonaws.com:5432/zombies';
 const db = pgp(connectionString);
 
 module.exports = db;
